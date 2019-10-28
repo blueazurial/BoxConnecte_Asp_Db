e7c74cbc1ccf5bfc83e030275f1681e3a706d92a
de goes maud <maud1405@hotmail.com>
2019-10-28 02:51:27 +0100

    ajout du theme cosmic dans angular

--- angular-box/src/app/app.module.ts
+++ angular-box/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+//ng module est une declaration de temps les component module et providers que l app 
+//vas utiliser 
+// un module c'est une sorte de librairie si on veux l utilisé on doit l importer 
+// dans le ng module 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NbLayoutModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
